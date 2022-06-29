3883dd601c449adf257e6610c875eaf1a36e8799
강승훈 <iltk2000@gmail.com>
2022-06-29 15:04:41 +0900

    feat: 1.0.0 배포

---
+++ index.js
@@ -0,0 +1,42 @@
+const core = require('@actions/core')
+const puppeteer = require('puppeteer')
+
+try {
+  (async () => {
+    const target = core.getInput("target-file")
+  const slack_token = core.getInput('slack-token')
+  const channels = core.getInput('slack-channels')
+  const imgName = core.getInput('img-name') || 'test'
+  const imgType = core.getInput('img-type') || 'jpeg'
+
+  if (detailPath && slack_token && channels) {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    const path = `${imgName}.${imgType}`
+  
+    await page.goto(`file://${__dirname}/${target}`)
+    await page.waitForTimeout(1000)
+    await page.screenshot({
+      fullPage: true,
+      path,
+    })
+    const Axios = axios.create({
+      baseURL: 'https://slack.com/api',
+      headers: {
+        Authorization: `Bearer ${slack_token}`,
+      },
+    })
+  
+    await Axios.post('/files.upload', {
+      channels,
+      file: fs.createReadStream(path),
+    })
+  
+    await browser.close()
+  } 
+ 
+  })()
+} catch (error) {
+  console.log('error', error)
+  throw new Error("screenshot failed")
+}
