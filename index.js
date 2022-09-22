85961345478fd0b5cda9ac014baa209972d28a04
= <iltk2000@gmail.com>
2022-09-22 20:51:54 +0900

    fix: action의 기능을 변경함.

--- index.js
+++ index.js
@@ -1,52 +1,44 @@
 const core = require('@actions/core');
-const puppeteer = require('puppeteer');
-const { WebClient } = require('@slack/web-api');
 const fs = require('fs');
+const axios = require('axios');
 
 try {
   (async () => {
-    const target = core.getInput('target-file') || '/public/index.html';
+    const target = core.getInput('target-file-path');
     const slack_token =
       core.getInput('slack-token') ||
-      'xoxp-366190432502-725008683671-4078730998738-b342812f3b9f3fb528a128151b0ebd9a';
-    const channels = core.getInput('slack-channels') || 'DM948GFS4';
-    const title = core.getInput('img-name') || 'test';
+      'xoxp-366190432502-725008683671-4114705957909-a870d1e79904a9ea498639e2bcd9b9e2';
 
-    if (target && slack_token && channels) {
-      const web = new WebClient(slack_token);
-      console.log('puppeteer', puppeteer);
-      const browser = await puppeteer.launch({
-        args: [
-          '--no-sandbox',
-          '--disable-setuid-sandbox',
-          '--disable-dev-shm-usage',
-        ],
+    const channelId = core.getInput('slack-channels') || 'DM948GFS4';
+    const sendUrl =
+      core.getInput('send-url') ||
+      'https://automation-mathflat.vercel.app/api/cypress/postReport';
+    const fileType = core.getInput('file-type') || 'html';
+    const imgName = core.getInput('img-name') || 'test';
+    const imgType = core.getInput('img-type') || 'jpeg';
+
+    if (!slack_token || !channelId || !target) {
+      core.setFailed('필수값이 빠졌습니다. 확인해주세요.');
+      return;
+    }
+
+    const encodedData = `data:${type};base64,${fs.readFileSync(target, {
+      encoding: 'base64',
+    })}`;
+
+    try {
+      axios.post(sendUrl, {
+        channels: channelId,
+        encodedData,
+        fileType,
+        imgName,
+        imgType,
       });
-      const page = await browser.newPage();
-      const savePath = `${title}.jpeg`;
-
-      await page.goto(`file://${process.cwd()}${target}`);
-      await page.waitForTimeout(1000);
-      await page.screenshot({
-        fullPage: true,
-        path: savePath,
-      });
-
-      try {
-        await web.files.upload({
-          channels,
-          title,
-          file: fs.createReadStream(savePath),
-        });
-      } catch (error) {
-        console.log('error', error);
-        core.setFailed(error.message);
-      } finally {
-        await browser.close();
-      }
+    } catch (error) {
+      console.log('error', error);
+      core.setFailed(error.message);
     }
   })();
 } catch (error) {
-  console.log('error', error);
   core.setFailed(error.message);
 }
