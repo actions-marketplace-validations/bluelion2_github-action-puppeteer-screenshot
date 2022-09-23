e820ffcc20cd55e7e18074f33cb09ac09551893d
= <iltk2000@gmail.com>
2022-09-23 15:59:57 +0900

    fix: puppeteer-screenshot으로 롤백

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
-# github-action-Send-Base64
+# github-action-puppeteer-screenshot
 
-지정된 경로에 있는 파일을 base64로 변환해서 보내주는 액션입니다.
+puppeteer를 통해 파일을 캡쳐한 뒤 slack으로 보내는 액션입니다.
+
+- [puppeteer-screenshot-action](https://github.com/lannonbr/puppeteer-screenshot-action) 참고
 
 ---
 
 ## Option
 
-|      Option      | Require | Description                         |
-| :--------------: | :-----: | :---------------------------------- |
-| target-file-path |  true   | Base64로 변환할 파일 경로           |
-|     send-url     |  true   | base64로 변환한 파일을 리턴받을 url |
-|    file-type     |  false  | 결과물 이름 : Default - html        |
+|      Option      | Require | Description               |
+| :--------------: | :-----: | :------------------------ |
+| target-file-path |  true   | 캡쳐할 파일 절대경로      |
+|   slack-token    |  true   | slack oauth token         |
+|  slack-channels  |  true   | slack channel id          |
+|     img-name     |  false  | img 이름 : Default - test |
+|     img-type     |  false  | img 타입 : Default - jpeg |
+
+---
--- index.js
+++ index.js
@@ -1,37 +1,89 @@
 const core = require('@actions/core');
+const puppeteer = require('puppeteer-core');
+const { WebClient } = require('@slack/web-api');
 const fs = require('fs');
-const axios = require('axios');
+const os = require('os');
+const path = require('path');
+
+// https://github.com/lannonbr/puppeteer-screenshot-action
+function getChromePath() {
+  let browserPath;
+
+  const type = os.type;
+  switch (type) {
+    case 'Windows_NT': {
+      const programFiles =
+        os.arch() === 'x64'
+          ? process.env['PROGRAMFILES(X86)']
+          : process.env.PROGRAMFILES;
+      browserPath = path.join(
+        programFiles,
+        'Google/Chrome/Application/chrome.exe'
+      );
+      break;
+    }
+    case 'Linux': {
+      browserPath = '/usr/bin/google-chrome';
+      break;
+    }
+    case 'Darwin': {
+      browserPath =
+        '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
+      break;
+    }
+  }
+
+  if (browserPath && browserPath.length > 0) {
+    return path.normalize(browserPath);
+  }
+
+  throw new TypeError(`Cannot run action. ${os.type} is not supported.`);
+}
 
 try {
   (async () => {
-    const target = core.getInput('target-file-path');
-    const sendUrl = core.getInput('send-url');
-    const fileType = core.getInput('file-type') || 'html';
+    const targetPath = core.getInput('target-file-path');
+    const slack_token = core.getInput('slack-token');
+    const channels = core.getInput('slack-channels');
+    const title = core.getInput('img-name') || 'test';
+    const fileType = core.getInput('img-type') || 'jpeg';
 
-    if (!sendUrl || !target) {
-      core.setFailed('필수값이 빠졌습니다. 확인해주세요.');
-      return;
-    }
-
-    const path = `${process.cwd()}${target}`;
-
-    const encodedData = `data:${fileType};base64,${fs.readFileSync(path, {
-      encoding: 'base64',
-    })}`;
-
-    try {
-      axios.post(sendUrl, {
-        headers: {
-          'Content-Type': 'Application/x-www-form-urlencoded',
-        },
-        encodedData,
-        fileType,
+    if (targetPath && slack_token && channels) {
+      const web = new WebClient(slack_token);
+      const browser = await puppeteer.launch({
+        executablePath: getChromePath(),
       });
-    } catch (error) {
-      console.log('error', error);
-      core.setFailed(error.message);
+      const page = await browser.newPage();
+      const savePath = `${title}.${fileType}`;
+
+      await page.goto(
+        `${
+          process.env.GITHUB_WORKSPACE || `file://${process.cwd()}`
+        }${targetPath}`
+      );
+      // await page.goto(`file://${process.cwd()}${targetPath}`);
+      setTimeout(async () => {
+        await page.screenshot({
+          fullPage: true,
+          path: savePath,
+        });
+
+        try {
+          await web.files.upload({
+            channels,
+            title,
+            file: fs.createReadStream(savePath),
+          });
+        } catch (error) {
+          console.log('error', error);
+          core.setFailed(error.message);
+        } finally {
+          await browser.close();
+        }
+      }, 1000);
     }
   })();
 } catch (error) {
+  console.log('error', error);
   core.setFailed(error.message);
 }
